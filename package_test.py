375cc9d89fe46fb190d4490bc194d991cd3427bd
WeaponMaster <1925730614@qq.com>
2020-04-30 12:22:53 +0800

    Initial commit

---
+++ package_test.py
@@ -0,0 +1,8 @@
+# from package_test import *
+#
+# data = numpy.array([1,2,3])
+# print(data)
+
+from dir2pac import *
+data = np.array([1,2,3])
+print(data)
---
+++ package_test/__init__.py
@@ -0,0 +1,6 @@
+import numpy
+import pandas
+import os
+import sys
+
+__all__ = ['os', 'sys', 'numpy', 'pandas']
