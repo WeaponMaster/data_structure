35f2d03dddf7191b6c23c7478854018d4c248194
WeaponMaster <1925730614@qq.com>
2020-05-07 13:04:18 +0800

    修改linkstack部分代码

--- stack/linkstack.py
+++ stack/linkstack.py
@@ -1,9 +1,9 @@
-""" A Stack using a Linked List like structure """
-from typing import Any, Optional
+# 导入typing包,用作类型注解
+from typing import Any, Optional, NoReturn
 
 
 class Node:
-    def __init__(self, data: Any, next: Optional["Node"] = None):
+    def __init__(self, data: Any, next: Optional["Node"] = None): # next为可选类型,可为Node可为None
         self.data: Any = data
         self.next: Optional["Node"] = next
 
@@ -12,32 +12,28 @@ class Node:
 
 
 class LinkedStack:
-
-    def __init__(self) -> None:
+    def __init__(self) -> NoReturn:
         self.top: Optional[Node] = None
-
-    def is_empty(self) -> bool:
-        return self.top is None
 
     def push(self, item: Any) -> None:
         node: Node = Node(item)
-        if self.is_empty():
+        if self.top is None:
             self.top = node
         else:
-            # each node points to the item "lower" in the stack
             node.next = self.top
             self.top = node
 
     def pop(self) -> Any:
-        """ returns and removes item at top of stack """
-        if self.is_empty():
+        if self.top is None:
             raise IndexError("pop from empty stack")
         else:
-            # "remove" element by having top point to the next one
             assert isinstance(self.top, Node)
             node: Node = self.top
             self.top = node.next
             return node.data
+
+    def is_empty(self) -> bool:
+        return self.top is None
 
     def __repr__(self):  # String representation/visualization of a Linked Lists
         current = self.top
